ef57b53fa0f59be2d237d0fdb99ebb690eb16a24
John Dages <john_dages@msn.com>
2019-02-22 02:02:32 -0500

    web configs

--- App.py
+++ App.py
@@ -43,7 +43,7 @@ def TrainModel():
 
 @app.route("/")
 def HeartBeat():
-    return "HeartBeat"
+    return "--Service Active--"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int("80"), debug=True)
